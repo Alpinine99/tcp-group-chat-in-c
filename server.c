7bb99b04a791266686ae387e4a66125e5f59e887
Alpinine99 <andreprojects001@gmail.com>
2026-01-08 22:31:47 +0300

    one step fom completion of logic, remaining exclusion checking

--- server.c
+++ server.c
@@ -21,6 +21,7 @@ typedef struct {
 void close_server();
 void* handle_client(void* client_details);
 void broadcast_message(const char* message, const char* sender_alias);
+int resolve_client_list(clientDetails_t* buffer_out, const char* alias_list, int sender_socket);
 
 int server_id;
 int client_count = 0;
@@ -105,6 +106,9 @@ int main(int argc, char* argv[]) {
 }
 
 void close_server() {
+    char shutdown_msg[MAX_MESSAGE_LEN];
+    snprintf(shutdown_msg, sizeof(shutdown_msg), "%s", QUIT);
+    broadcast_message(shutdown_msg, SERVER_ALIAS);
     printf(YELLOW BOLD "\nInfo: " RESET "Shutting down server...\n");
     close(server_id);
     printf(GREEN BOLD "Success: " RESET "Server socket closed. Exiting.\n");
@@ -118,7 +122,7 @@ void* handle_client(void* client_details) {
 
     char alias[MAX_USERNAME_LEN];
     bool connected = false;
-    send(sock, "Please enter your alias: ", 24, 0);
+    send(sock, SERVER_ALIAS ": Please enter your alias? ", BUFFER_SIZE, 0);
     do {
         ssize_t bytes_received = recv(sock, alias, MAX_USERNAME_LEN, 0);
         if (bytes_received <= 0) {
@@ -147,7 +151,7 @@ void* handle_client(void* client_details) {
     } while (!connected);
     strncpy(CLIENTS[client_id].client_alias, alias, MAX_USERNAME_LEN);
     printf(GREEN BOLD "Success: " RESET "Client %d set alias to " YELLOW "'%s'.\n" RESET, client_id + 1, alias);
-    char join_msg[BUFFER_SIZE] = {0};
+    char join_msg[MAX_MESSAGE_LEN] = {0};
     snprintf(join_msg, sizeof(join_msg), "%s has joined the chat!", alias);
     broadcast_message(join_msg, SERVER_ALIAS);
 
@@ -179,7 +183,7 @@ void* handle_client(void* client_details) {
 
     client_count--;
     close(sock);
-    char leave_msg[BUFFER_SIZE] = {0};
+    char leave_msg[MAX_MESSAGE_LEN] = {0};
     snprintf(leave_msg, sizeof(leave_msg), "%s has left the chat.", alias);
     broadcast_message(leave_msg, SERVER_ALIAS);
     printf(YELLOW BOLD "Info: " RESET "Connection with client " YELLOW "'%s'" RESET " closed.\n", alias);
@@ -187,15 +191,43 @@ void* handle_client(void* client_details) {
 }
 
 void broadcast_message(const char* message, const char* sender_alias) {
-    clientDetails_t WHITE_LIST[MAX_CLIENTS];
+    /*
+    *message format:
+    *   <INCLUDE|EXCLUDE> alias1,alias2,... message_content
+    */
+
+    int sender_socket = 0;
+    if (!strcmp(sender_alias, SERVER_ALIAS) == 0) {
+        for (int i = 0; i < client_count; i++) {
+            if (strcmp(CLIENTS[i].client_alias, sender_alias) == 0) {
+                sender_socket = CLIENTS[i].client_socket;
+                break;
+            }
+        }
+    }
+    int white_list_count = MAX_CLIENTS;
+    clientDetails_t WHITE_LIST[MAX_CLIENTS] = {0};
 
     char message_copy[BUFFER_SIZE] = {0};
     char filtered_message[MAX_MESSAGE_LEN] = {0};
     strncpy(message_copy, message, BUFFER_SIZE);
     char* control_sequence = strtok(message_copy, " ");
-    snprintf(filtered_message, sizeof(filtered_message), "%s", message + strlen(control_sequence) + 1);
+
     if (strncmp(control_sequence, INCLUDE, strlen(INCLUDE)) == 0) {
         printf(YELLOW BOLD "Info: " RESET "Inclusion broadcast invoked.\n");
+        char* include_list = strtok(message_copy + (strlen(INCLUDE) + 1), " ");
+        snprintf(filtered_message, sizeof(filtered_message), BLUE BOLD "[@]" RESET "%s", message + strlen(control_sequence) + strlen(include_list) + 2);
+        if (resolve_client_list(WHITE_LIST, include_list, sender_socket) != 2) {
+            for (int i = 0; i < white_list_count; i++) {
+                if (strcmp(WHITE_LIST[i].client_alias, "") == 0) {
+                    white_list_count = i;
+                    break;
+                }
+                printf(YELLOW BOLD "Info: " RESET "verified alias '%s'\n" ,WHITE_LIST[i].client_alias); 
+            }
+        } else {
+            return;
+        }
     } else if (strncmp(control_sequence, EXCLUDE, strlen(EXCLUDE)) == 0) {
         printf(YELLOW BOLD "Info: " RESET "Exclusion broadcast invoked.\n");
     } else {
@@ -203,10 +235,96 @@ void broadcast_message(const char* message, const char* sender_alias) {
         strncpy(filtered_message, message, MAX_MESSAGE_LEN);
     }
 
-    printf(YELLOW BOLD "Info: " RESET "Broadcasting message %s from '%s'\n", filtered_message, sender_alias);
+    const char* sender = strcmp(sender_alias, SERVER_ALIAS) == 0 ? "(server)" : sender_alias;
+    printf(YELLOW BOLD "Info: " RESET "Broadcasting message %s from '%s'\n", filtered_message, sender);
     char formatted_message[BUFFER_SIZE] = {0};
     snprintf(formatted_message, sizeof(formatted_message), "%s: %s", sender_alias, filtered_message);
-    for (int i = 0; i < client_count; i++) {
+    for (int i = 0; i < white_list_count; i++) {
         send(WHITE_LIST[i].client_socket, formatted_message, strlen(formatted_message), 0);
     }
 }
+
+int resolve_client_list(clientDetails_t* buffer_out, const char* alias_list, int sender_socket) {
+    /*
+    * alias_list format:
+    *   alias1,alias2,alias3,...
+    */
+
+    int warning = 0;
+    char list[strlen(alias_list)];
+    strncpy(list, alias_list, strlen(alias_list));
+    int count = 0;
+    int list_len = 0;
+    char invalid_aliases[client_count*(MAX_USERNAME_LEN+1)];
+    strcpy(invalid_aliases, "");
+    
+    if (strcmp(&alias_list[strlen(alias_list) - 1], ",") == 0) {
+        printf(RED BOLD "Error: " RESET "invalid syntax\n");
+        strcat(invalid_aliases, "invalid syntax");
+        warning = 2;
+    } else if (!strstr(alias_list, ",")) {
+        const char* alias = alias_list;
+        bool client_found = false;
+        for (int i=0;i<client_count;i++) {
+            if (strcmp(CLIENTS[i].client_alias, alias) == 0) {
+                buffer_out[0] = CLIENTS[i];
+                client_found = true;
+                break;
+            }
+        }
+        if (!client_found) {
+            printf(YELLOW BOLD "Warning: " RESET "Alias '%s' not found in client list. Skipping.\n", alias);
+            char user_name[MAX_USERNAME_LEN] = {0};
+            strcat(user_name, alias);
+            strcat(invalid_aliases, user_name);
+            warning = 1;
+        }
+    } else {
+        while (list_len <= (int) strlen(alias_list)) {
+            char new_list[strlen(alias_list)];
+            snprintf(new_list, sizeof(new_list), "%s", list + list_len);
+            char* alias = strtok(new_list, ",");
+            if (list_len + strlen(alias) > strlen(alias_list)) {
+                snprintf(new_list, sizeof(new_list), "%s", alias_list + list_len);
+                alias = strtok(new_list, "");
+            }
+            list_len += strlen(alias) + 1;
+            
+            bool client_found = false;
+            for (int i = 0; i < client_count; i++) {
+                if (strcmp(CLIENTS[i].client_alias, alias) == 0) {
+                    buffer_out[count++] = CLIENTS[i];
+                    client_found = true;
+                    break;
+                }
+            }
+            if (!client_found) {
+                printf(YELLOW BOLD "Warning: " RESET "Alias '%s' not found in client list. Skipping.\n", alias);
+                char user_name[MAX_USERNAME_LEN] = {0};
+                strcat(user_name, alias);
+                strcat(user_name, ", ");
+                strcat(invalid_aliases, user_name);
+                warning = 1;
+            }
+        }
+    }
+
+    char return_message[BUFFER_SIZE];
+    if (warning == 0) {
+        char message[MAX_MESSAGE_LEN];
+        snprintf(message, sizeof(message), GREEN BOLD "[success!] " RESET "aliases verified.\n");
+        snprintf(return_message, sizeof(return_message), "%s: %s", SERVER_ALIAS, message);
+    } else if (warning == 1) {
+        char message[MAX_MESSAGE_LEN];
+        snprintf(message, sizeof(message), YELLOW BOLD "[Warning!] " RESET "aliases (%s) not found\n" , invalid_aliases);
+        snprintf(return_message, sizeof(return_message), "%s: %s", SERVER_ALIAS, message);
+    } else {
+        char message[MAX_MESSAGE_LEN];
+        snprintf(message, sizeof(message), RED BOLD "[Error!] " RESET "%s\n" , invalid_aliases);
+        snprintf(return_message, sizeof(return_message), "%s: %s", SERVER_ALIAS, message);
+    }
+    
+    send(sender_socket, return_message, strlen(return_message), 0);
+    return warning;
+}
+
